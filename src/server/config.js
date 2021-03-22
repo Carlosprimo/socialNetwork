3f8f606688ea1c2213117af0f54035d91bb75a79
CarlosPrimo <carlosprimob@gmail.com>
2021-03-21 19:29:48 -0500

    Configure los middlewares

--- src/server/config.js
+++ src/server/config.js
@@ -1,7 +1,9 @@
 const path = require('path'); //modulo path me permite unir directorios 
 const express = require('express');
 const route = require('../routes/index');
-
+const ejs = require('ejs');
+const morgan = require('morgan');
+const multer = require('multer');
 
 module.exports = app => {
 
@@ -11,10 +13,15 @@ module.exports = app => {
         // Motor de plantilla
     app.set("view engine", "ejs");
     app.set("views", path.join( __dirname , '../views'));
-
-
     
-
+    ejs.views = ['partials','layouts']; 
+    app.set('view engine', 'ejs');
+    
+    // middlewares
+    app.use(morgan('dev'));
+    app.use(multer({
+        dest: path.join(__dirname, '../public/upload/temp')
+    })).single('files');
     //routes
     route(app);
 
@@ -23,7 +30,4 @@ module.exports = app => {
     app.use('/public' , express.static(path.join(__dirname , '../public')) );
 
     return app;
-    
-
-
 }
